fe390d9c8a374fe2a6cc9e58d161b4509e15b7e0
Svynct <rtkiller006@gmail.com>
2023-12-22 12:04:07 -0300

    Initial Commit

---
+++ AluraViagensSwiftUI/AluraViagensSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AluraViagensSwiftUIApp.swift
+//  AluraViagensSwiftUI
+//
+//  Created by Ricardo dos Santos Amaral on 22/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AluraViagensSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
